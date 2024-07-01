5238548d4bd12d1788d06a41336c4f59155364e6
MacBook Pro 2015 <macbookpro2015@MacBooks-MacBook-Pro.local>
2024-07-01 21:03:56 +0600

    page Route Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from "./pages/HomePage";
+import Item from "./pages/Item";
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/item" element={<Item />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
